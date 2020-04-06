6c3241a565617125a3d5fa30fa2df588d53231d4
fon <FonTo_85@hotmail.com>
2020-04-06 09:40:10 +0200

    mer öningar

--- src/app.js
+++ src/app.js
@@ -1,23 +1,23 @@
 class Drone {
     constructor(id, name) {
-       this._id = id;
-       this._name = name;
+        this._id = id;
+        this._name = name;
     }
 
-    get id(){
+    get id() {
         return this._id;
     }
 
-    set id(id){
+    set id(id) {
         this._id = id;
     }
 
-   fly(){
-       console.log("Drone " + this.id + " is flying");
-   }
-   static getCompany(){
-       console.log("in company");
-   }
+    fly() {
+        console.log("Drone " + this.id + " is flying");
+    }
+    static getCompany() {
+        console.log("in company");
+    }
 }
 
 
@@ -27,12 +27,99 @@ class Drone {
 let drone = new Drone("a123", "flyer");
 let drone2 = new Drone("b456", "jump");
 
-console.log(drone.id);
-drone.id = "5555";
-console.log(drone.id);
+
+const helloworld = () => [1, 2, 3, 45];
+
+const dragonEvents = [{
+        type: "attack",
+        value: 12,
+        target: "player-dorkman"
+    },
+    {
+        type: "yawn",
+        value: 40
+    },
+    {
+        type: "eat",
+        target: "horse"
+    },
+    {
+        type: "attack",
+        value: 23,
+        target: "player-fluffykins"
+    },
+    {
+        type: "attack",
+        value: 12,
+        target: "player-dorkman"
+    },
+]
+
+const totalDamageOnDorkman = dragonEvents
+    .filter(function isAttack(event) {
+        return event.type === "attack"
+    })
+    .filter(function isDorkman(event) {
+        return event.target === "player-dorkman"
+    })
+    .map(function (value) {
+        return value.value
+    })
+    .reduce(function (prev, value) {
+        return (prev || 0) + value;
+    })
 
 
+console.log(totalDamageOnDorkman);
 
+
+const totalDamageOnDorkman2 = dragonEvents
+    .filter(event => event.type === "attack")
+    .filter(event => event.target === "player-dorkman")
+    .map(event => event.value)
+    .reduce((prev, value) => (prev || 0) + value);
+
+console.log(totalDamageOnDorkman2)
+
+
+const list  = [1,2,3,4,5]
+const newList = list.map((value)=> value*2)
+
+console.log(newList)
+
+const anotherList = list.map( (value) =>"hi");
+
+console.log(anotherList);
+
+
+const filterList = list.filter((value) => value > 2 );
+
+console.log(filterList)
+
+const anotherList2 = list.filter((value)=> false);
+console.log(anotherList2)
+
+
+const person = {
+    name: 'Earthling Ed',
+    age: 29,
+    location: 'London',
+    mood: 'Content',
+    likes: ['animals', 'people', 'talking']
+   } 
+
+const {name,age,location} = person;
+
+console.log(name,age,location);
+
+
+const lista = ['a', 'b', 'c'];
+console.log(...lista); // 'a' 'b' 'c'
+
+
+const person2 = { name: 'Will', age: 55, location: 'America' }
+
+console.log(...person2)
 
 // console.log(typeof drone);
 // console.log(drone instanceof Drone);
